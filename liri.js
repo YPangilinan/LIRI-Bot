61a39169fe70550b47dd6ae47f1c0b7f972821c0
Yssabel Pangilinan <yssabelpangilinan@yahoo.com>
2020-06-10 18:19:33 -0700

    creating multiple functions

--- liri.js
+++ liri.js
@@ -8,15 +8,40 @@ var keys = require("./keys");
 var Spotify = require('node-spotify-api');
 var spotify = new Spotify(keys.spotify);
 
+//require moment
+var moment = require("moment");
+
+//require FS
+var fs = require("fs");
+
 //initialize axios
 var axios = require("axios");
 
+//user commands and query
 var command = process.argv[2];
 var userQuery = process.argv.slice(3).join(" ");
 
-//Conditionals for commands
+//CONDITIONALS FOR COMMANDS
+function userCommands(command,userQuery){
+    if(command == "spotify-this-song"){
+        spotifySearch();
+    }
+    else if(command == "movie-this"){
+        movieSearch();
+    }
+    else if(command == "concert-this"){
+        concertSearch();
+    }
+    else if(command == "do-what-it-says"){
+        doSay();
+    }
+};
 
-if (command == "spotify-this-song"){
+userCommands(command,userQuery);
+
+//SPOTIFY SEARCH
+
+function spotifySearch(){
     console.log("You searched for: " + userQuery);
 
     if (!userQuery){
@@ -42,44 +67,102 @@ if (command == "spotify-this-song"){
    });
 }
 
-if (command == "movie-this"){
-
-    console.log("You searched for: " + userQuery);
-
-    if (!userQuery){
-    userQuery = "mr nobody";
-    console.log("If you havent, watched 'Mr. Nobody', then you should: http://www.imdb.com/title/tt0485947/");
-    console.log("It's on Netflix!");
-    };
+//OMDB SEARCH
+function movieSearch(){
+        console.log("You searched for: " + userQuery);
     
-    axios.get("http://www.omdbapi.com/?t=" + userQuery + "&y=&plot=short&apikey=trilogy").then(
-        function(response) {
-          console.log("Title: " + response.data.Title);
-          console.log("Year: " + response.data.Year);
-          console.log("IMDB Rating: " + response.data.imdbRating);
+        if (!userQuery){
+        userQuery = "mr nobody";
+        console.log("If you havent, watched 'Mr. Nobody', then you should: http://www.imdb.com/title/tt0485947/");
+        console.log("It's on Netflix!");
+        };
+        
+        axios.get("http://www.omdbapi.com/?t=" + userQuery + "&y=&plot=short&apikey=trilogy").then(
+            function(response) {
+              console.log("Title: " + response.data.Title);
+              console.log("Year: " + response.data.Year);
+              console.log("IMDB Rating: " + response.data.imdbRating);
+    
+              var rottenTomatoes = response.data.Ratings;
+              if(rottenTomatoes.length >2){
+                  console.log((rottenTomatoes[1].Source) + " Rating:" + (rottenTomatoes[1].Value));
+              };
+              console.log("Country: " + response.data.Country);
+              console.log("Language: " + response.data.Language);
+              console.log("Plot: " + response.data.Plot);
+              console.log("Actors: " + response.data.Actors); 
+            })
+            .catch(function(error) {
+              if (error.response) {
+                console.log("---------------Data---------------");
+                console.log(error.response.data);
+                console.log("---------------Status---------------");
+                console.log(error.response.status);
+                console.log("---------------Status---------------");
+                console.log(error.response.headers);
+              } else if (error.request) {
+                console.log(error.request);
+              } else {
+                console.log("Error", error.message);
+              }
+              console.log(error.config);
+            });      
+    };
 
-          var rottenTomatoes = response.data.Ratings;
-          if(rottenTomatoes.length >2){
-              console.log((rottenTomatoes[1].Source) + " Rating:" + (rottenTomatoes[1].Value));
-          };
-          console.log("Country: " + response.data.Country);
-          console.log("Language: " + response.data.Language);
-          console.log("Plot: " + response.data.Plot);
-          console.log("Actors: " + response.data.Actors); 
-        })
-        .catch(function(error) {
-          if (error.response) {
-            console.log("---------------Data---------------");
-            console.log(error.response.data);
-            console.log("---------------Status---------------");
-            console.log(error.response.status);
-            console.log("---------------Status---------------");
-            console.log(error.response.headers);
-          } else if (error.request) {
-            console.log(error.request);
-          } else {
-            console.log("Error", error.message);
-          }
-          console.log(error.config);
-        });      
-};
+//BANDSINTOWN SEARCH
+function concertSearch(){
+        console.log("You searched for: " + userQuery);
+        if (!userQuery){
+            userQuery = "Celine Dion";
+            console.log("If you dont know Celine Dion, you should!");
+            console.log("----------------------");
+            };
+    
+        axios.get("https://rest.bandsintown.com/artists/" + userQuery + "/events?app_id=codingbootcamp").then(
+    
+            function(response){
+                // console.log(response.data);
+                var concertArray = response.data;
+                for(var i=0; i<concertArray.length; i++){
+                    console.log("Venue Name: " + concertArray[i].venue.name);
+                    console.log("Venue Location: " + concertArray[i].venue.location);
+                    var concertDate = moment(concertArray[i].datetime).format("MM/DD/YYYY hh:00 A");
+                    console.log("Date and Time: " + concertDate);
+                    console.log("----------------------");
+                }
+            })
+            .catch(function(error) {
+                if (error.response) {
+                  console.log("---------------Data---------------");
+                  console.log(error.response.data);
+                  console.log("---------------Status---------------");
+                  console.log(error.response.status);
+                  console.log("---------------Status---------------");
+                  console.log(error.response.headers);
+                } else if (error.request) {
+                  console.log(error.request);
+                } else {
+                  console.log("Error", error.message);
+                }
+                console.log(error.config);
+              });      
+    };
+
+//DO WHAT IT SAYS
+function doSay(){
+
+        fs.readFile("random.txt", "utf8", function(error,data){
+            if (error){
+                return console.log(error);
+            }
+            console.log(data);
+    
+            var dataArray = data.split(",");
+    
+            command = dataArray[0];
+            userQuery = dataArray[1];
+
+            userCommands(command,userQuery);
+        });
+    };
+
